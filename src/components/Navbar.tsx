a0b321def10a6ca7ed598286d2c0fb433aa1d5a2
Arghya <arghya2801@gmail.com>
2025-05-10 13:08:05 +0530

    Modified Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,56 +1,31 @@
-import React from 'react'
-// import {
-//     NavigationMenu,
-//     // NavigationMenuContent,
-//     // NavigationMenuIndicator,
-//     NavigationMenuItem,
-//     // NavigationMenuLink,
-//     NavigationMenuList,
-//     NavigationMenuTrigger,
-//     // NavigationMenuViewport,
-// } from "@/components/ui/navigation-menu"
+import React from 'react';
+import Link from 'next/link';
 
-
-const Navbar = () => {
-    return (
-        <>
-            <div className="h-20 sticky top-0 flex justify-between items-center gap-x-4 px-20 text-4xl bg-gradient-to-b from-background to-opacity-50">
-                <div> </div>
-                <nav className="flex gap-x-4">
-                    <a href="#" className="relative hover:underline group">
-                        <span className="group-hover:underline group-hover:decoration-2 group-hover:underline-offset-4">Home</span>
-                        <span className="absolute -bottom-1 left-0 w-full h-1 bg-gradient-to-r from-transparent to-primary transition duration-300 group-hover:w-full group-hover:h-0.5" />
-                    </a>
-                    <a href="#" className="relative hover:underline group">
-                        <span className="group-hover:underline group-hover:decoration-2 group-hover:underline-offset-4">Projects</span>
-                        <span className="absolute -bottom-1 left-0 w-full h-1 bg-gradient-to-r from-transparent to-primary transition duration-300 group-hover:w-full group-hover:h-0.5" />
-                    </a>
-                    <a href="#" className="relative hover:underline group">
-                        <span className="group-hover:underline group-hover:decoration-2 group-hover:underline-offset-4">About</span>
-                        <span className="absolute -bottom-1 left-0 w-full h-1 bg-gradient-to-r from-transparent to-primary transition duration-300 group-hover:w-full group-hover:h-0.5" />
-                    </a>
-                </nav>
-                {/* <NavigationMenu>
-                    <NavigationMenuList className='text-4xl'>
-                        <NavigationMenuItem>
-                            <NavigationMenuTrigger>Home</NavigationMenuTrigger>
-                        </NavigationMenuItem>
-                        <NavigationMenuItem>
-                            <NavigationMenuTrigger>About</NavigationMenuTrigger>
-                        </NavigationMenuItem>
-                        <NavigationMenuItem>
-                            <NavigationMenuTrigger>Blog</NavigationMenuTrigger>
-                        </NavigationMenuItem>
-                        <NavigationMenuItem>
-                            <NavigationMenuTrigger>Contact</NavigationMenuTrigger>
-                        </NavigationMenuItem>
-                    </NavigationMenuList>
-                </NavigationMenu> */}
-            </div>
-
-        </>
-
-    )
+interface NavItemProps {
+  href: string;
+  label: string;
 }
 
-export default Navbar
+const NavItem: React.FC<NavItemProps> = ({ href, label }) => {
+  return (
+    <Link 
+      href={href}
+      className="relative group text-gray-900 dark:text-gray-100 hover:text-gray-700 dark:hover:text-gray-300 transition duration-200 text-lg font-medium px-2 py-1"
+    >
+      {label}
+      <span className="absolute left-0 bottom-0 h-0.5 w-0 bg-gradient-to-r from-blue-500 to-purple-600 group-hover:w-full transition-all duration-300 ease-in-out" />
+    </Link>
+  );
+};
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="flex justify-end gap-x-6 py-4 pr-32">
+      <NavItem href="/" label="Home" />
+      <NavItem href="/projects" label="Projects" />
+      <NavItem href="/about" label="About" />
+    </nav>
+  );
+};
+
+export default Navbar;
